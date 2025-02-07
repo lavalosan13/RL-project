7a00f371a61e622d3f00197ee77b83577ce42544
Hugo Laval <hug.laval@live.fr>
2025-02-07 10:03:20 +0100

    class drone

--- app_renf.py
+++ app_renf.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers
+#from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
